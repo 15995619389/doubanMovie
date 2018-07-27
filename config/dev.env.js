43a3375671315aa3f09c89e74ce533dd3161a435
zuolihong <zuolihong@techina2025.com>
2018-07-27 16:37:52 +0800

    修改配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,9 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
 
-  // 知乎数据接口
+  // 本地测试环境请求后台接口的域名
   // API_ROOT:' http://news-at.zhihu.com/api'
-
-  // 豆瓣数据接口
-  // API_ROOT:'http://api.douban.com/v2'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+   // 上传服务器以后,请求后台接口的域名
+  // API_ROOT:' http://news-at.zhihu.com/api'
 }
